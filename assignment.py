e0654feb82ea4096b27c353e9fd8fb6f1481bf22
holiday-inn <113055314+holiday-inn@users.noreply.github.com>
2023-09-15 12:21:48 -0700

    maze no fun :(

--- assignment.py
+++ assignment.py
@@ -1,8 +1,48 @@
- "help", "copyright", "credits" or "license" for more information.
->>> import turtle
->>> s = turtle.getscreen()
->>> t= turtle.Turtle
->>> t.fd(200)
->>> t.forward(50)
->>> t.lt(30)
->>> t.forward(35)
+import turtle
+import time
+
+
+t = turtle.Turtle()
+t.speed(10)
+t.hideturtle()
+s = turtle.Turtle()
+s.speed(10)
+s.hideturtle()
+s.pencolor("red")
+m = turtle.Turtle()
+m.speed(10) 
+m.hideturtle()
+m.pensize(2)
+h = turtle.Turtle()
+h.speed(10)
+h.hideturtle()
+h.pensize(5)
+t.penup()
+t.goto(0,-200)
+t.pendown()
+t.begin_fill()
+t.circle(200)
+def seconds():
+    s.clear()
+    s.setheading(90+(-sec*6))
+    s.forward(190)
+    s.backward(190)
+def minutes():
+    m.clear()
+    m.setheading(90+(-min*6))
+    m.forward(190)
+    m.backward(190)
+def hours():
+    h.clear()
+    h.setheading(90+(-hour*30))
+    h.forward(130)
+    h.backward(130)
+
+while True:
+    ti = time.localtime()
+    sec = int(time.strftime("%S",ti))
+    min = int(time.strftime("%M",ti))
+    hour = int(time.strftime("%H",ti))
+    seconds()
+    minutes()
+    hours()
---
+++ drawing.py
@@ -0,0 +1,10 @@
+import time
+'''t = time.localtime()
+sec = time.strftime("%S",t)
+min = time.strftime("%M",t)
+hour = time.strftime("%H",t)
+print(sec,min,hour)'''
+import turtle
+t = turtle.Turtle()
+while True:
+    time.sleep(9)
